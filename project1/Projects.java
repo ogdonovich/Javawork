0a22609ac50f522e12f4edc524e2923c824042f4
Steven Messler <95991339+ogdonovich@users.noreply.github.com>
2022-04-07 19:43:48 -0500

    Add files via upload

---
+++ project1/Projects.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class Projects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//variables list
+		String strName = "Steven Messler";
+		int numAge = 30;
+		String strGoal = "learn industry Java skills and secure employment";
+		String strOutput = "Hello, my name is %s. I am %d years old and I want to %s.";
+		
+		//Output
+		System.out.printf(strOutput, strName, numAge, strGoal);
+	}
+
+}
